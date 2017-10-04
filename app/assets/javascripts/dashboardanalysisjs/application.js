ffdce7ac1a7b7f58273d2e516083fd9120c51a95
isarceaux <isarceaux>
2017-10-04 22:48:55 +0200

    small edits due to kevin input on landing page

--- app/assets/javascripts/dashboardanalysisjs/application.js
+++ app/assets/javascripts/dashboardanalysisjs/application.js
@@ -1,1 +1,27 @@
-//= require_tree .
+//= require jquery
+//= require bootstrap.min
+//= require pace.min
+//= require modernizr.min
+//= require wow.min
+//= require jquery.nicescroll
+//= require dashboardanalysisjs/moment-2.2.1
+
+
+
+//= require jquery.app
+//= require dashboardanalysisjs/morris.min
+//= require dashboardanalysisjs/raphael.min
+//= require dashboardanalysisjs/morris.init
+//= require dashboardanalysisjs/c3
+//= require dashboardanalysisjs/d3.v3.min
+//= require dashboardanalysisjs/c3-chart.init
+
+/* ==============================================
+             Counter Up
+             =============================================== */
+            jQuery(document).ready(function($) {
+                $('.counter').counterUp({
+                    delay: 100,
+                    time: 1200
+                });
+            });
