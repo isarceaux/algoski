4ada462fea51b1f38b32e92753730b1bec32eba4
isarceaux <isarceaux>
2017-10-17 17:40:04 +0200

    didn't do much

---
+++ app/assets/javascripts/dashboardmapjs/application.js
@@ -0,0 +1,10 @@
+//= require jquery
+//= require bootstrap.min
+//= require pace.min
+//= require modernizr.min
+//= require wow.min
+//= require jquery.nicescroll
+//= require jquery.app
+
+//= require dashboardmapjs/gmaps
+//= require dashboardmapjs/gmaps-plugin
