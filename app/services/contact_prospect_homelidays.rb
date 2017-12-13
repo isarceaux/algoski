20641227094846ec0fe12330cfd64cc1b5fc2880
isarceaux <isarceaux>
2017-12-13 14:32:09 +0100

    started the bot to automatically contact owners

---
+++ app/services/contact_prospect_homelidays.rb
@@ -0,0 +1,55 @@
+class ContactProspectHomelidays
+  
+  def initialize
+    browser = Watir::Browser.new(:firefox)
+    browser.goto 'https://www.abritel.fr/location-vacances/p1477842'
+    sleep(20)
+    puts "sleep finished should refresh now"
+    browser.refresh
+    class_button = 'btn cta js-emailOwnerButton btn-sm btn-link btn-inquiry-link'
+    browser.link(class: class_button).click
+    # binding.pry
+
+    # Filling the form
+
+    # id_flexible = 'modal-flexibleDateInput'
+    # browser.link(id: id_flexible).click
+
+    fill_first_name = browser.text_field(id: 'modal-inquirerFirstName')
+    fill_first_name.send_keys('Isabelle')
+
+    fill_last_name = browser.text_field(id: 'modal-inquirerLastName')
+    fill_last_name.send_keys('Bonnet')
+
+    fill_mail = browser.text_field(id: 'modal-inquirerEmailAddress')
+    fill_mail.send_keys('isabellecorp@gmail.com')
+
+    fill_text = browser.text_field(id: 'modal-comments')
+    fill_text.send_keys("test")
+
+    validation_button_class = 'btn btn-primary js-submitInquiry'
+    browser.link(class: validation_button_class).click
+
+  end
+
+  def new_text
+    new_text = ""
+    new_text = "Bonjour,
+
+Connaissez-vous le site Algoski : www.algo.ski ? 
+
+Il permet aux propriétaires d'appartements au ski de louer plus de semaines et au meilleur prix en leur permettant de se comparer aux autres propriétaires dans leur station !
+
+De nombreux propriétaires à la montagne en France et en Suisse ont amélioré leur taux de remplissage et leurs revenus grâce à cet outil simple mais efficace. En un coup d'oeil vous obtenez les prix moyens de location pour chaque semaine !
+
+Si vous souhaitez en savoir plus n'hésitez pas à nous contacter par mail. Le mail est disponible sur la page contact de notre site.
+
+Au plaisir de vous aider dans vos locations,
+
+Isabelle
+Pour Algoski "
+    return new_text
+  end
+
+end
+
--- app/services/contat_prospect_homelidays.rb
+++
@@ -1,3 +0,0 @@
-browser = Watir::Browser.new(:firefox)
-
-browser.goto 'https://www.abritel.fr/location-vacances/p1397998'
