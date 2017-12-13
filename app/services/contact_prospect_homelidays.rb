a62c989b26b27c53170ae98976e716ebfaaaa086
isarceaux <isarceaux>
2017-12-13 17:39:33 +0100

    ready for a test deployed

--- app/services/contact_prospect_homelidays.rb
+++ app/services/contact_prospect_homelidays.rb
@@ -1,45 +1,48 @@
 class ContactProspectHomelidays
   
-  def initialize
-    browser = Watir::Browser.new(:firefox)
+  attr_accessor :browser
 
-    browser.goto 'https://www.abritel.fr/location-vacances/p1477842'
-    sleep(10)
+  def initialize
+    @browser = Watir::Browser.new(:firefox)
+    perform(Housing.find(23157).link)
+    perform(Housing.find(23156).link)
+  end
+
+  def perform(url_client)
+
+    @browser.goto 'https://www.abritel.fr'+url_client
+    sleep(5)
     puts "sleep finished should refresh now"
-    browser.refresh
+    @browser.refresh
     class_button = 'btn cta js-emailOwnerButton btn-sm btn-link btn-inquiry-link'
-    browser.link(class: class_button).click
+    @browser.link(class: class_button).click
     # binding.pry
 
     # Filling the form
 
     #Checking the box flexible date of stay
     checkbox_id = "modal-flexibleDateInput"
-    browser.checkbox(id:checkbox_id).click
+    @browser.checkbox(id:checkbox_id).click
 
     #Adding one adult
     increment_button_class = "btn btn-default counter-button counter-button--gray js-increment"
-    browser.button(class:increment_button_class).click
+    @browser.button(class:increment_button_class).click
 
     #Filling information
-    fill_first_name = browser.text_field(id: 'modal-inquirerFirstName')
+    fill_first_name = @browser.text_field(id: 'modal-inquirerFirstName')
     fill_first_name.send_keys('Isabelle')
 
-    fill_last_name = browser.text_field(id: 'modal-inquirerLastName')
+    fill_last_name = @browser.text_field(id: 'modal-inquirerLastName')
     fill_last_name.send_keys('Bonnet')
 
-    fill_mail = browser.text_field(id: 'modal-inquirerEmailAddress')
+    fill_mail = @browser.text_field(id: 'modal-inquirerEmailAddress')
     fill_mail.send_keys('isabellecorp@gmail.com')
 
     #Writting the mail
     text_id = "modal-comments"
-    browser.textarea(id:text_id).send_keys(new_text)
+    @browser.textarea(id:text_id).send_keys(new_text)
 
-    puts "Everything is ready for submitting the form... just check while the program sleeps"
-    # sleep(5)
-    binding.pry
-    
-    #Sending the form
+    #Sending the form (commented for tests)
     validation_button_class = 'btn btn-primary js-submitInquiry'
     browser.button(class: validation_button_class).click
 
