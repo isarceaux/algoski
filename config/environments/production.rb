2a5b55d331527564bcf744c71cba6a25f972129c
isarceaux <isarceaux>
2018-02-05 18:19:14 +0100

    changed mailer to use sendgrid in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,23 +94,37 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #Configuring mailer to work in production
-  config.action_mailer.default_url_options = { protocol: 'https', :host => 'algo.ski' }
+  # config.action_mailer.default_url_options = { protocol: 'https', :host => 'algo.ski' }
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.default :charset => "utf-8"
+
+  # config.action_mailer.smtp_settings = 
+  # {
+  #   address: ENV['OVH_DOMAIN'],
+  #   port: 587,
+  #   domain: ENV["OVH_DOMAIN"],
+  #   authentication: "plain",
+  #   :enable_starttls_auto => true,
+  #   :openssl_verify_mode  => 'none',
+  #   user_name: ENV["OVH_USERNAME"],
+  #   password: ENV["OVH_PASSWORD"]
+  # }
+
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default :charset => "utf-8"
-
-  config.action_mailer.smtp_settings = 
-  {
-    address: ENV['OVH_DOMAIN'],
-    port: 587,
-    domain: ENV["OVH_DOMAIN"],
-    authentication: "plain",
-    :enable_starttls_auto => true,
-    :openssl_verify_mode  => 'none',
-    user_name: ENV["OVH_USERNAME"],
-    password: ENV["OVH_PASSWORD"]
+  config.action_mailer.delivery_method = :smtp
+  host = '<your heroku app>.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
   }
 
 end
